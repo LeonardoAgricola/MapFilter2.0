edc3b64b011db426e39c443bc67c3106f70048ed
LeonardoAgricola <maldanerlf@hotmail.com>
2024-08-01 21:34:46 -0300

    modificado para salvar somente lat, long e os valores do atributo filtrado

--- project/src/pacotes/SalvarArquivos.java
+++ project/src/pacotes/SalvarArquivos.java
@@ -1,13 +1,12 @@
-package pacotes;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class SalvarArquivos {
 
-    public void salvar(String[][] matriz, int n, int col, File nome, String[] cabecalho, int est) {
+    public void salvar(ArrayList<ArrayList<Double>> matriz, File nome, int est) {
         String ext = null;
         if (est == 1) {
             ext = ".csv";
@@ -16,17 +15,25 @@ public class SalvarArquivos {
             ext = ".txt";
         }
         try {
-            //nome.createNewFile();
-            FileWriter file = new FileWriter(nome+ext, false);
+            int n = matriz.size();
+            String[] header = new String[] { "lat", "long", "values" };
+
+            FileWriter file = new FileWriter(nome + ext, false);
             try (BufferedWriter escrever = new BufferedWriter(file)) {
-                for (int i = 1; i < col+1; i++) {
-                    escrever.write(cabecalho[i] + ",");
+                int count = 0;
+                for (String h : header) {
+                    h = (count < 2) ? h + "," : h;
+                    escrever.write(h);
+                    count++;
                 }
-               // escrever.write("Fileira,");
-                for (int i = 1; i <= n; i++) {
+                for (int i = 0; i < n; i++) {
                     escrever.newLine();
-                    for (int j = 0; j < col; j++) {
-                        escrever.write(matriz[i][j] + ",");
+                    count = 0;
+                    for (int j = 0; j < 3; j++) {
+                        String value = String.valueOf(matriz.get(i).get(j));
+                        value = (count < 2) ? value + "," : value;
+                        escrever.write(value);
+                        count++;
                     }
                 }
                 escrever.flush();
