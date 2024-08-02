8485b9a08e8fee87f5df4d1766dee1d3e6e490ef
LeonardoAgricola <maldanerlf@hotmail.com>
2024-08-01 21:25:29 -0300

    ignorar .vscode e outra pastas de projeto

--- project/src/pacotes/FiltroGeral.java
+++ project/src/pacotes/FiltroGeral.java
@@ -1,61 +1,43 @@
-package pacotes;
-
-import java.util.Arrays;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Leonardo Felipe Maldaner
- * @author Mark Spekken
- * @author Jose Paulo Molin
- */
+import java.util.stream.Collectors;
+import java.util.ArrayList;
 
 public class FiltroGeral {
 
-    public String[][] filtrogeral(String[][] matriz, int x, int y,
-            int nCol, int colAt, double var) {
+    /**
+     * Filters the input matrix based on the specified variance.
+     * Only rows with the third column's value within the specified range
+     * (median +/- variance percentage) are retained.
+     *
+     * @param matrix   The input matrix to be filtered.
+     * @param variance The variance percentage used to determine the range.
+     * @return The filtered matrix.
+     */
+    public ArrayList<ArrayList<Double>> filterMatrix(ArrayList<ArrayList<Double>> matrix, double variance) {
+        int n = matrix.size();
+        variance = variance / 100;
 
-        int n = (int) Double.parseDouble(matriz[0][0]);
-        String[][] filtrados = new String[n + 1][nCol + 1];
-        double[] valores = new double[n + 1];
-        double limI;
-        double limS;
-        double mediana = 0;
-        int cont = 0;
-        int nmediana;
-     
-        var = var / 100;//transforma em decimal
-        for (int i = 1; i < n; i++) {
-            valores[i] = Double.parseDouble(matriz[i][colAt]);
-        }
-        Arrays.sort(valores, 1, n);
+        // Get the values of the third column and sort them
+        ArrayList<Double> values = matrix.stream()
+                .map(row -> row.get(2))
+                .sorted()
+                .collect(Collectors.toCollection(ArrayList::new));
 
-        nmediana = (int) Math.round(n * 0.50);
-      
-        for (int i = 2; i < n - 1; i++) {
-            if (i == nmediana) {
-                mediana = valores[i];
-            }
-        }
-    
-        limI = mediana - (mediana * var);
-        limS = mediana + (mediana * var);
-        
-        int conta1 = 0;
-        for (int i = 1; i < n; i++) {
-            if ((Double.parseDouble(matriz[i][colAt]) <= limI)
-                    || (Double.parseDouble(matriz[i][colAt]) >= limS)) {
-            } else {
-                conta1++;
-                System.arraycopy(matriz[i], 0, filtrados[conta1], 0, nCol + 1);
-            }
-        }
+        // Calculate the median
+        int medianIndex = (int) Math.round(n * 0.50);
+        double median = values.get(medianIndex);
 
-        filtrados[0][0] = String.valueOf(conta1);
-        return filtrados;
+        // Define lower and upper limits
+        double lowerLimit = median - (median * variance);
+        double upperLimit = median + (median * variance);
+
+        // Filter the matrix
+        ArrayList<ArrayList<Double>> filteredMatrix = matrix.stream()
+                .filter(row -> {
+                    double value = row.get(2);
+                    return value > lowerLimit && value < upperLimit;
+                })
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        return filteredMatrix;
     }
 }
